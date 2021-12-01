59d61e5223e997aad088debb449f486d7ce17b97
Muhammed Saifur Rahman <saifur.rahman@adnsl.net>
2021-12-01 18:01:39 +0600

    init

---
+++ lib/Model/member.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Member {
+
+   String image;
+   String name;
+   String designation;
+   String quotes;
+
+  Member(
+      {
+    required this.image,
+    required this.name,
+    required this.designation,
+    required this.quotes
+  }
+  );
+
+
+}
---
+++ lib/splash.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_project/HomeScreen.dart';
+import 'package:progress_indicators/progress_indicators.dart';
+
+class Splash extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => InitState();
+
+}
+
+class InitState extends State<Splash> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async{
+    var duration = Duration(seconds: 5);
+    return new Timer(duration, homePageRoute);
+  }
+
+  homePageRoute(){
+   Navigator.pushReplacement(context, MaterialPageRoute(
+     builder: (context) => HomeScreen()
+   ));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return initWidget();
+  }
+
+  Widget initWidget() {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              child: Image.asset("images/app_logo.png", width: 200, height: 200),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 40.0),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: JumpingDotsProgressIndicator(
+                    fontSize: 50.0,
+                      numberOfDots: 5,
+                    color: new Color(0xFF29A74A),
+                    milliseconds: 120,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin:  EdgeInsets.only(bottom: 10.0),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text('কারিগরি সহায়তায় ADN Telecom', style: TextStyle(color: Colors.black)),
+                )
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+
+
+}
