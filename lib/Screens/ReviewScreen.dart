7b67e8798582168265c60ac3b3e780579794e313
Muhammed Saifur Rahman <saifur.rahman@adnsl.net>
2021-12-13 18:18:41 +0600

    Post Api list view 80% done

--- lib/Screens/ReviewScreen.dart
+++ lib/Screens/ReviewScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/Api/http_service.dart';
+import 'package:flutter_project/Model/Status.dart';
 import 'package:flutter_project/Model/member.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter_project/Services/api_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,13 +14,17 @@ State<StatefulWidget> createState() => InitState();
 
 class InitState extends State<ReviewPage> {
   var name="", token, mobile="";
-  var res;
+  Status? res;
+  String? date, message;
   late HttpService httpService;
   ApiService apiService = ApiService();
 
+
   @override
   void initState(){
-    getStatus();
+    setState(() {
+      getStatus();
+    });
     super.initState();
   }
 
@@ -37,7 +43,7 @@ return initWidget();
         child: Padding(
           padding: const EdgeInsets.only(left: 10.0, top: 2.0, right: 10.0, bottom: 2.0),
           child: ListView.builder(
-            itemCount: 6,
+            itemCount: res!.data!.length,
             itemBuilder: (context, i) {
              return listWidget(i);
             },
@@ -47,16 +53,41 @@ return initWidget();
   );
   }
 
+  String? getDateString(obg, index){
+    return obg?.data?[index].date;
+
+  }
+
+  String? getMessageString(obg, index){
+    return obg?.data?[index].message;
+
+  }
+
   Widget listWidget(int index) {
+    date = res?.data?[index].date;
+   /* var dmyString = '23/4/1999';
+    var get_date = DateFormat('d/M/yyyy').parse(dmyString);
+
+    print("FormattedDate: "+ DateFormat.yMMMMd().format(get_date));*/
  return Card(
    elevation: 5,
    shape: RoundedRectangleBorder(
      borderRadius: BorderRadius.circular(10),
    ),
-   child: ListTile(
-     title: Text("Date"),
-     subtitle: Text("Details of date"),
-   ),
+   child: Column(
+     crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Padding(
+             padding: const EdgeInsets.all(5.0),
+             child: Text('$date'),
+           ),
+           Padding(
+             padding: const EdgeInsets.all(5.0),
+             child: Text("newwwwwwwwwwwwwwwwwwwwwwwwwwwww", style: new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),),
+           )
+         ],
+       ),
+
  );
   }
 
@@ -64,8 +95,10 @@ return initWidget();
     httpService = HttpService();
     SharedPreferences preferences = await SharedPreferences.getInstance();
     print("what Token: " + preferences.getString("token")!);
-    res = await apiService.CurrentStatus(preferences.getString("token")!);
-    print("OutPut " +  res.toString());
+    res = (await apiService.CurrentStatus(preferences.getString("token")!));
+
+
+    print("OutPut " +  res!.data!.length.toString());
   }
 
 }
--- lib/Services/api_service.dart
+++ lib/Services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_project/Model/Status.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 class ApiService{
@@ -45,7 +46,7 @@ class ApiService{
   }
 
   var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNDgxODg3NDZmN2FjNDIwNmRkNWU2ZGRhYmJiOGI5MDI2ZjljZGNjMGRjYTY5MDA0MWMyNGNiY2I4ZDFkYzlmMWNjNTAzMGQ2NmM5NTc0ZjkiLCJpYXQiOjE2Mzg2NzgwNjYuMjk3NTU3MSwibmJmIjoxNjM4Njc4MDY2LjI5NzU2MzEsImV4cCI6MTY3MDIxNDA2Ni4yODQ3MzIxLCJzdWIiOiI0MTk2NCIsInNjb3BlcyI6W119.lZO-ZUqCSq8Myoz78lJ863vDKvTvZnhV3W2GyLRKNHBbsdPwvsxHpPFbcoSCqW9q1k5opeulO_PSpsukgtfNyhY3KjZOzg6bT5IWH9SRBC7RL05z9tk_Up1g-r1VL5ltkL5VHGRLg3KwfW_H98vUun7gdCCwjJOPHP-gm9UZA5lN-2U0JC3VaWIKeChp3zG77VGC5nfirWgezjya2dc7sEVqZ7x63rP9QPzp5JA7w2w9pDEZ5R-mnOOudTQiJNbc1w6VcvDestaGAX3ndRf2dfH4InYtNR6zHsrdfwK-gAwsiAauI0nh7DVrQ_sijUNyCs0xCHZTaxshZ1SULIaB0d3suhZUSQG0zVrHzS_MtdTovoyPRV6G171JGRH_-TPRvwNxhdLSYvVJRWXTAOr8VrNiDydXJkgUZ7ecFJAFt6LYbxJT_iSTEM94H4IBzGwFZP6ik4lkTe0Z1cQliZBL_cNiFR_Sqq3ocsvz_MSyPhb3R-U33u8KrnRcGwVY9HZCoDX1L02Ye8-J1yKzHLELC8yctoFrHOOARMSJbPCdOt0WIgjSo4UtZtDwAXcK8qQn5z_223UExFsgIhm7Nc3SRElPN4w0IzeHwoyj0KHbh8MZtjTLMgUiIYdEIID9Rz5un176RexsXS2f6MTPFwo6aidRYsohsuo7QNf9imlM3sI";
-  Future<String> CurrentStatus(token) async{
+  Future<Status?> CurrentStatus(token) async{
     print("Api_Service: "+token);
     try{
       var response = await http.post(
@@ -58,14 +59,14 @@ class ApiService{
       if(response.statusCode == 200){
         //success
         print("Registration: "+response.body);
-        return response.body;
+        return Status.fromJson(jsonDecode(response.body));;
       }else{
         print("resCode: "+ response.statusCode.toString());
-        return "error1";
+        return null;
       }
     }catch(e){
       print("resError: "+ e.toString());
-      return "error2";
+      return null;
     }
   }
 }
